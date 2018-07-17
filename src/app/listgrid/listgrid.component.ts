248b9869d5bf5056aa639da7f0af204f630e0dd4
Arti Dhanotiya <artidhanotiya2189@gmail.com>
2018-07-17 01:15:21 -0700

    filters in listgrid done

--- src/app/listgrid/listgrid.component.ts
+++ src/app/listgrid/listgrid.component.ts
@@ -20,7 +20,6 @@ export class ListgridComponent implements OnInit {
   // sort:string = "";
   sortValue: string = "1";
 
-  pizzaFromIndex1 = "";
 
   constructor(private userService: UserService) { }
   ngOnInit() {
@@ -49,127 +48,195 @@ export class ListgridComponent implements OnInit {
 
     if (this.keyword != "" && this.state == "" && this.city == "" && this.categories == "" && this.sortValue == "1") {
       console.log("sort = " + this.sortValue);
-      this.check = "1001";
+      this.check = "10001";
       this.userService.getDataByKeyword(this.keyword).subscribe(data => { this.users = data; });
+      console.log(this.users);
     }
-    else if (this.keyword = "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "1") {
-      this.check = "0101";
+    else if (this.keyword == "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "1") {
+      this.check = "01001";
       this.userService.getDataByState(this.state).subscribe(data => { this.users = data; });
+      console.log(this.users);
     }
-    else if (this.keyword = "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "1") {
-      this.check = "0011";
+    else if (this.keyword == "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "1") {
+      this.check = "00101";
       this.userService.getDataByCity(this.city).subscribe(data => { this.users = data; });
+      console.log(this.users);
     }
-    else if (this.keyword = "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "1101";
+    else if (this.keyword == "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "1") {
+      this.check = "00011";
       this.userService.getDataByCategories(this.categories).subscribe(data => { this.users = data; });
+
     }
     else if (this.keyword != "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "1") {
-      this.check = "1011";
+      this.check = "11001";
       this.userService.getDataByKeywordAndState(this.keyword, this.state).subscribe(data => { this.users = data; });
     }
     else if (this.keyword != "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "1") {
-      this.check = "0111";
+      this.check = "10101";
       this.userService.getDataByKeywordAndCity(this.keyword, this.city).subscribe(data => { this.users = data; });
     }
     else if (this.keyword != "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "1111";
+      this.check = "10011";
       this.userService.getDataByKeywordAndCategories(this.keyword, this.categories).subscribe(data => { this.users = data; });
     }
-    else if (this.keyword = "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "1") {
-      this.check = "0101";
+    else if (this.keyword == "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "1") {
+      this.check = "01101";
       this.userService.getDataByStateAndCity(this.state,this.city).subscribe(data => { this.users = data; });
     }
-    else if (this.keyword = "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "0011";
+    else if (this.keyword == "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "1") {
+      this.check = "01011";
       this.userService.getDataByStateAndCategories(this.state,this.categories).subscribe(data => { this.users = data; });
     }
-    else if (this.keyword = "" && this.state == "" && this.city != "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "1101";
+    else if (this.keyword == "" && this.state == "" && this.city != "" && this.categories != "" && this.sortValue == "1") {
+      this.check = "00111";
       this.userService.getDataByCityAndCategories(this.city,this.categories).subscribe(data => { this.users = data; });
     }
     else if (this.keyword != "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "1") {
-      this.check = "1011";
+      this.check = "11101";
       this.userService.getDataByKeywordStateAndCity(this.keyword,this.state,this.city).subscribe(data => { this.users = data; });
     }
     else if (this.keyword != "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "0111";
+      this.check = "11011";
       this.userService.getDataByKeywordStateAndCategories(this.keyword, this.state, this.categories).subscribe(data => { this.users = data; });
     }
     else if (this.keyword == "" && this.state != "" && this.city != "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "1111";
+      this.check = "01111";
       this.userService.getDataByStateCityAndCategories(this.state, this.city, this.categories).subscribe(data => { this.users = data; });
     }
     else if (this.keyword != "" && this.state != "" && this.city != "" && this.categories != "" && this.sortValue == "1") {
-      this.check = "1101";
+      this.check = "11111";
       this.userService.getDataByKeywordStateCityAndCategories(this.keyword,this.state,this.city,this.categories).subscribe(data => { this.users = data; });
     }
    
+//sorting high to low---------------------------------------------
+
+else if (this.keyword != "" && this.state == "" && this.city == "" && this.categories == "" && this.sortValue == "2") {
+  console.log("sort = " + this.sortValue);
+  this.check = "10002";
+  this.userService.getDataByKeywordH(this.keyword).subscribe(data => { this.users = data; });
+  console.log(this.users);
+}
+else if (this.keyword == "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "01002";
+  this.userService.getDataByStateH(this.state).subscribe(data => { this.users = data; });
+  console.log(this.users);
+}
+else if (this.keyword == "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "00102";
+  this.userService.getDataByCityH(this.city).subscribe(data => { this.users = data; });
+  console.log(this.users);
+}
+else if (this.keyword == "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "00012";
+  this.userService.getDataByCategoriesH(this.categories).subscribe(data => { this.users = data; });
+
+}
+else if (this.keyword != "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "11002";
+  this.userService.getDataByKeywordAndStateH(this.keyword, this.state).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "10102";
+  this.userService.getDataByKeywordAndCityH(this.keyword, this.city).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "10012";
+  this.userService.getDataByKeywordAndCategoriesH(this.keyword, this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "01102";
+  this.userService.getDataByStateAndCityH(this.state,this.city).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "01012";
+  this.userService.getDataByStateAndCategoriesH(this.state,this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state == "" && this.city != "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "00112";
+  this.userService.getDataByCityAndCategoriesH(this.city,this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "11102";
+  this.userService.getDataByKeywordStateAndCityH(this.keyword,this.state,this.city).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "11012";
+  this.userService.getDataByKeywordStateAndCategoriesH(this.keyword, this.state, this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state != "" && this.city != "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "01112";
+  this.userService.getDataByStateCityAndCategoriesH(this.state, this.city, this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state != "" && this.city != "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "11112";
+  this.userService.getDataByKeywordStateCityAndCategoriesH(this.keyword,this.state,this.city,this.categories).subscribe(data => { this.users = data; });
+}
+
+//sorting low to high -------------------------------------------------------------
+else if (this.keyword != "" && this.state == "" && this.city == "" && this.categories == "" && this.sortValue == "2") {
+  console.log("sort = " + this.sortValue);
+  this.check = "10003";
+  this.userService.getDataByKeywordL(this.keyword).subscribe(data => { this.users = data; });
+  console.log(this.users);
+}
+else if (this.keyword == "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "01003";
+  this.userService.getDataByStateL(this.state).subscribe(data => { this.users = data; });
+  console.log(this.users);
+}
+else if (this.keyword == "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "00103";
+  this.userService.getDataByCityL(this.city).subscribe(data => { this.users = data; });
+  console.log(this.users);
+}
+else if (this.keyword == "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "00013";
+  this.userService.getDataByCategoriesL(this.categories).subscribe(data => { this.users = data; });
+
+}
+else if (this.keyword != "" && this.state != "" && this.city == "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "11003";
+  this.userService.getDataByKeywordAndStateL(this.keyword, this.state).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state == "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "10103";
+  this.userService.getDataByKeywordAndCityL(this.keyword, this.city).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state == "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "10013";
+  this.userService.getDataByKeywordAndCategoriesL(this.keyword, this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "01103";
+  this.userService.getDataByStateAndCityL(this.state,this.city).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "01013";
+  this.userService.getDataByStateAndCategoriesL(this.state,this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state == "" && this.city != "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "00113";
+  this.userService.getDataByCityAndCategoriesL(this.city,this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state != "" && this.city != "" && this.categories == "" && this.sortValue == "2") {
+  this.check = "11103";
+  this.userService.getDataByKeywordStateAndCityL(this.keyword,this.state,this.city).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state != "" && this.city == "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "11013";
+  this.userService.getDataByKeywordStateAndCategoriesL(this.keyword, this.state, this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword == "" && this.state != "" && this.city != "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "01113";
+  this.userService.getDataByStateCityAndCategoriesL(this.state, this.city, this.categories).subscribe(data => { this.users = data; });
+}
+else if (this.keyword != "" && this.state != "" && this.city != "" && this.categories != "" && this.sortValue == "2") {
+  this.check = "11113";
+  this.userService.getDataByKeywordStateCityAndCategoriesL(this.keyword,this.state,this.city,this.categories).subscribe(data => { this.users = data; });
+}
 
 
-    // sorting ascending-------------------------------------------------------------
-
-   /* else if (this.city != "" && this.restaurant == "" && this.pizza == "" && this.sortValue == "2") {
-      console.log("sort = " + this.sortValue);
-      this.check = "1002";
-      this.userService.getDataByCitySortA(this.city).subscribe(data => { this.users = data; });
-    }
-    else if (this.city == "" && this.restaurant != "" && this.pizza == "" && this.sortValue == "2") {
-      this.check = "0102";
-      this.userService.getDataByRestaurantSortA(this.restaurant).subscribe(data => { this.users = data; });
-    }
-    else if (this.city == "" && this.restaurant == "" && this.pizza != "" && this.sortValue == "2") {
-      this.check = "0012";
-      this.userService.getDataByPizzaSortA(this.pizza).subscribe(data => { this.users = data; });
-    }
-    else if (this.city != "" && this.restaurant != "" && this.pizza == "" && this.sortValue == "2") {
-      this.check = "1102";
-      this.userService.getDataByCityAndRestaurantSortA(this.city, this.restaurant).subscribe(data => { this.users = data; });
-    }
-    else if (this.city != "" && this.restaurant == "" && this.pizza != "" && this.sortValue == "2") {
-      this.check = "1012";
-      this.userService.getDataByCityAndPizzaSortA(this.city, this.pizza).subscribe(data => { this.users = data; });
-    }
-    else if (this.city == "" && this.restaurant != "" && this.pizza != "" && this.sortValue == "2") {
-      this.check = "0112";
-      this.userService.getDataByRestaurantAndPizzaSortA(this.restaurant, this.pizza).subscribe(data => { this.users = data; });
-    }
-    else if (this.city != "" && this.restaurant != "" && this.pizza != "" && this.sortValue == "2") {
-      this.check = "1112";
-      this.userService.getDataByCityRestaurantAndPizzaSortA(this.city, this.restaurant, this.pizza).subscribe(data => { this.users = data; });
-    }
-
-    // sorting descending-----------------------------------------------------------
-
-    else if (this.city != "" && this.restaurant == "" && this.pizza == "" && this.sortValue == "3") {
-      console.log("sort = " + this.sortValue);
-      this.check = "1003";
-      this.userService.getDataByCitySortD(this.city).subscribe(data => { this.users = data; });
-    }
-    else if (this.city == "" && this.restaurant != "" && this.pizza == "" && this.sortValue == "3") {
-      this.check = "0103";
-      this.userService.getDataByRestaurantSortD(this.restaurant).subscribe(data => { this.users = data; });
-    }
-    else if (this.city == "" && this.restaurant == "" && this.pizza != "" && this.sortValue == "3") {
-      this.check = "0013";
-      this.userService.getDataByPizzaSortD(this.pizza).subscribe(data => { this.users = data; });
-    }
-    else if (this.city != "" && this.restaurant != "" && this.pizza == "" && this.sortValue == "3") {
-      this.check = "1103";
-      this.userService.getDataByCityAndRestaurantSortD(this.city, this.restaurant).subscribe(data => { this.users = data; });
-    }
-    else if (this.city != "" && this.restaurant == "" && this.pizza != "" && this.sortValue == "3") {
-      this.check = "1013";
-      this.userService.getDataByCityAndPizzaSortD(this.city, this.pizza).subscribe(data => { this.users = data; });
-    }
-    else if (this.city == "" && this.restaurant != "" && this.pizza != "" && this.sortValue == "3") {
-      this.check = "0113";
-      this.userService.getDataByRestaurantAndPizzaSortD(this.restaurant, this.pizza).subscribe(data => { this.users = data; });
-    }
-    else if (this.city != "" && this.restaurant != "" && this.pizza != "" && this.sortValue == "3") {
-      this.check = "1113";
-      this.userService.getDataByCityRestaurantAndPizzaSortD(this.city, this.restaurant, this.pizza).subscribe(data => { this.users = data; });
-    }*/
+   
   }
 
 
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -82,6 +82,102 @@ export class UserService {
     return this.http.get<any>(this.baseUrl + "keyword_state_city_categories/" + keyword + "/" + state + "/" + city + "/" + categories);
   }
 
+// sorting from high to low
+
+getDataByKeywordH(keyword: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_high_to_low/" + keyword);
+}
+getDataByStateH(state: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_high_to_low/" + state);
+}
+getDataByCityH(city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "city_high_to_low/" + city);
+}
+getDataByCategoriesH(categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "categories_high_to_low/" + categories);
+}
+getDataByKeywordAndStateH(keyword: string, state: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_high_to_low/" + keyword + "/" + state);
+}
+getDataByKeywordAndCityH(keyword: string, city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_city_high_to_low/" + keyword + "/" + city);
+}
+getDataByKeywordAndCategoriesH(keyword: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_categories_high_to_low/" + keyword + "/" + categories);
+}
+getDataByStateAndCityH(state: string, city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_city_high_to_low/" + state + "/" + city);
+}
+getDataByStateAndCategoriesH(state: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_categories_high_to_low/" + state + "/" + categories);
+}
+getDataByCityAndCategoriesH(city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "city_categories_high_to_low/" + city + "/" + categories);
+}
+getDataByKeywordStateAndCityH(keyword: string, state: string, city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_city_high_to_low/" + keyword + "/" + state + "/" + city);
+}
+getDataByKeywordStateAndCategoriesH(keyword: string, state: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_categories_high_to_low/" + keyword + "/" + state + "/" + categories);
+}
+
+getDataByKeywordCityAndCategoriesH(keyword: string, city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_city_categories_high_to_low/" + keyword + "/" + city + "/" + categories);
+}
+getDataByStateCityAndCategoriesH(state: string,city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_city_categories_high_to_low/" + state + "/" + city + "/" + categories);
+}
+getDataByKeywordStateCityAndCategoriesH(keyword: string, state: string,city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_city_categories_high_to_low/" + keyword + "/" + state + "/" + city + "/" + categories);
+}
 
 
+//sort from low to high
+
+getDataByKeywordL(keyword: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_low_to_high/" + keyword);
+}
+getDataByStateL(state: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_low_to_high/" + state);
+}
+getDataByCityL(city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "city_low_to_high/" + city);
+}
+getDataByCategoriesL(categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "categories_low_to_high/" + categories);
+}
+getDataByKeywordAndStateL(keyword: string, state: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_low_to_high/" + keyword + "/" + state);
+}
+getDataByKeywordAndCityL(keyword: string, city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_city_low_to_high/" + keyword + "/" + city);
+}
+getDataByKeywordAndCategoriesL(keyword: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_categories_low_to_high/" + keyword + "/" + categories);
+}
+getDataByStateAndCityL(state: string, city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_city_low_to_high/" + state + "/" + city);
+}
+getDataByStateAndCategoriesL(state: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_categories_low_to_high/" + state + "/" + categories);
+}
+getDataByCityAndCategoriesL(city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "city_categories_low_to_high/" + city + "/" + categories);
+}
+getDataByKeywordStateAndCityL(keyword: string, state: string, city: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_city_low_to_high/" + keyword + "/" + state + "/" + city);
+}
+getDataByKeywordStateAndCategoriesL(keyword: string, state: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_categories_low_to_high/" + keyword + "/" + state + "/" + categories);
+}
+
+getDataByKeywordCityAndCategoriesL(keyword: string, city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_city_categories_low_to_high/" + keyword + "/" + city + "/" + categories);
+}
+getDataByStateCityAndCategoriesL(state: string,city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "state_city_categories_low_to_high/" + state + "/" + city + "/" + categories);
+}
+getDataByKeywordStateCityAndCategoriesL(keyword: string, state: string,city: string, categories: string): Observable<any> {
+  return this.http.get<any>(this.baseUrl + "keyword_state_city_categories_low_to_high/" + keyword + "/" + state + "/" + city + "/" + categories);
+}
 }
